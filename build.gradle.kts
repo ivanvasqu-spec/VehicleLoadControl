13925d44c5c3ab1357fd6e67b42913b93b0e233e
ivanvasqu-spec <ivanvasqu@gmail.com>
2026-07-26 20:14:34 -0400

    Add root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    kotlin("android") version "1.9.22" apply false
+    kotlin("jvm") version "1.9.22" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+task("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
